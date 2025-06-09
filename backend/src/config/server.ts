f8821c3b07c0842dbe6261523448f61d8f990a96
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2025-06-09 16:04:03 +0530

    feat: add cors

--- backend/src/config/server.ts
+++ backend/src/config/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { authorized } from "../middleware/auth_middleware";
 import userRouter from "../router/user_router";
 import authRouter from "../router/auth_router";
@@ -12,6 +13,18 @@ app.use(cookieParser());
 app.use("/user", authorized, userRouter);
 app.use("/auth", authRouter);
 app.use("/stake", stakeRouter);
+const corsOptions = {
+  origin: (_origin: any, callback: (arg0: null, arg1: boolean) => void) => {
+    // Check if the origin is in the list of allowed origins
+    callback(null, true);
+  },
+  //   origin: "http://localhost:3000", // Change this to the specific domain you want to allow
+  methods: "GET,PUT,POST,OPTIONS", // Specify allowed methods
+  credentials: true,
+  allowedHeaders: "*",
+};
+
+app.use(cors(corsOptions));
 /**
  * Convert
  *  - quote
